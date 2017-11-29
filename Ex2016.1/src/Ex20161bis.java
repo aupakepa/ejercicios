e15d23c2f7cc009dfe35e4c6808f88265d90c0b6
“aupakepa” <jmonforte@fontecabras.es>
2017-11-30 00:13:05 +0100

    fechas de examen de 2016

---
+++ Ex2016.1/src/Ex20161bis.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ex20161bis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+	}
+
+}
